d304778da8cc4f939cb1c2365d1fa8949e7d345a
Richard Murray <114324221+RMurray22@users.noreply.github.com>
2023-01-16 18:00:57 -0800

    added js and css file, updated README and finalized the html

---
+++ README.md
@@ -0,0 +1,3 @@
+The Impossible Quiz
+Hello and welcome to the hardest quiz on the internet. 
+All seriousness aside this is a very lighthearted quiz that I hope will you bring some laughter into your day.
---
+++ script.js
@@ -0,0 +1,16 @@
+const questions = [
+    {
+        questionText: "Where does Micky Mouse live?:",
+        options: ["1. The North Pole", "2. Disneyland", "3. The South Pole", "4. The Death Star"],
+        answer: "2. Disneyland",
+    },
+    {
+        questionText: "What is at the center of the soloar system",
+        options: [
+            "1. You",
+            "2. Me",
+            "3. Someone else",
+            "4. The Sun",
+        ],
+        answer: "4. The Sun",
+    }
