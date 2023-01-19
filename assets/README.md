8428377ee752031a6ba16da18c23d796ad9bb83d
Richard Murray <114324221+RMurray22@users.noreply.github.com>
2023-01-18 18:26:21 -0800

    added correct questions and answers to code

--- assets/README.md
+++ assets/README.md
@@ -1,4 +1,4 @@
 The Impossible Quiz
 Hello and welcome to the hardest quiz on the internet. 
 All seriousness aside this is a very lighthearted quiz that I hope will you bring some laughter into your day.
-This took alot of time and was not without it's frustrations, I hope you enjoy this 2 question test as I ran out of time to think of more.
+I would have added a screenshot but I'm new to Macs and have no idea how to do it. (Work in progress).
--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,42 @@
 const questions = [
     {
-        questionText: "Where does Micky Mouse live?:",
-        options: ["1. The North Pole", "2. Disneyland", "3. The South Pole", "4. The Death Star"],
-        answer: "2. Disneyland",
+        questionText: "Commonly used data types DO NOT include:",
+        options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        answer: "3. alerts",
     },
     {
-        questionText: "What is at the center of the soloar system:",
+        questionText: "Arrays in JavaScript can be used to store ______.",
         options: [
-            "1. You",
-            "2. Me",
-            "3. Someone else",
-            "4. The Sun",
+            "1. numbers and strings",
+            "2. other arrays",
+            "3. booleans",
+            "4. all of the above",
         ],
-        answer: "4. The Sun",
+        answer: "4. all of the above",
     },
-
-
+    {
+        questionText:
+            "String values must be enclosed within _____ when being assigned to variables.",
+        options: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+        answer: "3. quotes",
+    },
+    {
+        questionText:
+            "A very useful tool used during development and debugging for printing content to the debugger is:",
+        options: [
+            "1. JavaScript",
+            "2. terminal/bash",
+            "3. for loops",
+            "4. console.log",
+        ],
+        answer: "4. console.log",
+    },
+    {
+        questionText:
+            "Which of the following is a statement that can be used to terminate a loop, switch or label statement?",
+        options: ["1. break", "2. stop", "3. halt", "4. exit"],
+        answer: "1. break",
+    },
 ];
 
 const startCard = document.querySelector("#start-card");
